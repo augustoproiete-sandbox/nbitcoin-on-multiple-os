51cc54579e8cc3e494d60639cc165d24e65786ea
C. Augusto Proiete <augusto@proiete.com>
2021-09-26 20:07:56 -0300

    Initial version

---
+++ run.cmd
@@ -0,0 +1,8 @@
+@echo on
+@cd %~dp0
+
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+set DOTNET_CLI_TELEMETRY_OPTOUT=1
+set DOTNET_NOLOGO=1
+
+dotnet run --project src/NBitcoinConsoleApp -c Release %*
---
+++ src/NBitcoinConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using NBitcoin;
+
+namespace NBitcoinConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateSimpleKeys();
+        }
+
+        private static void GenerateSimpleKeys()
+        {
+            var keys = new[]
+            {
+                new Key(fCompressedIn: true),
+                new Key(fCompressedIn: false),
+            };
+
+            var networks = new[]
+            {
+                Network.Main,
+                Network.TestNet,
+            };
+
+            var keysWithNetworks =
+            from k in keys
+            from n in networks
+            select new
+            {
+                Key = k,
+                Network = n,
+                Wif = k.GetWif(n),
+            };
+
+            foreach (var knw in keysWithNetworks)
+            {
+                Console.WriteLine($"Network: {knw.Network}");
+                Console.WriteLine($"Is Compressed: {knw.Key.IsCompressed}");
+                Console.WriteLine($"Private Key: {knw.Wif}");
+                Console.WriteLine($"Public Address (Legacy): {knw.Wif.GetAddress(ScriptPubKeyType.Legacy)}");
+                Console.WriteLine($"Public Address (Segwit): {knw.Wif.GetAddress(ScriptPubKeyType.Segwit)}");
+                Console.WriteLine($"Public Address (SegwitP2SH): {knw.Wif.GetAddress(ScriptPubKeyType.SegwitP2SH)}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
